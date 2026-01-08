4e1d6e3df04659ce325363eeb3bede1fd02f5ef2
Снежок <139562158+Kroshkos@users.noreply.github.com>
2026-01-08 22:14:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Конфигурация
-API_TOKEN = os.getenv('7330339638:AAGTMJmANqau4-7ucGW8wTa_rPMb_f5uVF8')
+API_TOKEN = '7330339638:AAGTMJmANqau4-7ucGW8wTa_rPMb_f5uVF8'
 if not API_TOKEN:
     logger.error("TELEGRAM_BOT_TOKEN не установлен!")
     raise ValueError("TELEGRAM_BOT_TOKEN не установлен!")
